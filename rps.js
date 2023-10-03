611505ee9ae38b3dbc56ce3437c622bea24af5fe
vinalvrm <vinalvrm@gmail.com>
2023-10-03 21:14:28 +1300

    created function to play 1 round using if statements

--- rps.js
+++ rps.js
@@ -8,9 +8,31 @@ function getComputerChoice() {
 
 
 // Create a function playRound that plays a single round of Rock Paper Scissors. Function takes two parameters playerSelection, computerSelection.
-// returns a string that declares winner of round like so: "You Lose! Paper beats Rock"
+// returns a string that declares winner of round like so: 'You Lose! Paper beats Rock'
 // Make playerSelection case-insensitive 
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
 
+    if (playerSelection === computerSelection) {
+        return 'Tie!'
+    // Lose
+    } else if (playerSelection == 'rock' && computerSelection == 'paper') {
+            return 'You Lose! Paper beats Rock'
+    } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+        return 'You Lose! Scissors beats Paper'
+    } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+        return 'You Lose! Rock beats Scissors'
+    } // Win 
+    else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        return 'You Win! Rock beats Scissors'
+
+    } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        return 'You Win! Paper beats Rock'
+        
+    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        return 'You Win! Scissors beats Paper'
+    };
+}
 
 
 // NOTE: return results, NOT console.log()
