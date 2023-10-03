329aec51c51b0a11e5d189799b96ace474aad86c
vinalvrm <vinalvrm@gmail.com>
2023-10-03 20:21:38 +1300

    added comments as guidelines to code

--- rps.js
+++ rps.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+// Create a function getComputerChoice that randomly returns 'Rock', 'Paper', 'Scissors'
+
+// Create a function playRound that plays a single round of Rock Paper Scissors. Function takes two parameters playerSelection, computerSelection.
+// returns a string that declares winner of round like so: "You Lose! Paper beats Rock"
+// Make playerSelection case-insensitive 
+
+// NOTE: return results, NOT console.log()
+
+// Create function game()
+// Use the previous function inside this one to play 5 rounds (loop), keeps track of score and reports winner and loser at end.
+// Use console.log() to display result of each round and winner
+// Use prompt() to get input from the user
