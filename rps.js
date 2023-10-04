36b8cade26d2197e7e9fb5d943176f4e03749a42
vinalvrm <vinalvrm@gmail.com>
2023-10-04 18:41:12 +1300

    creating main game function foundations

--- rps.js
+++ rps.js
@@ -40,3 +40,28 @@ function playRound(playerSelection, computerSelection) {
 // Use the previous function inside this one to play 5 rounds (loop), keeps track of score and reports winner and loser at end.
 // Use console.log() to display result of each round and winner
 // Use prompt() to get input from the user
+function game() {
+    let i = 0
+    let playScore = 0
+    let compScore = 0
+    while (i < 5) {
+        let playerChoice = prompt("Choose your weapon")
+        let compChoice = getComputerChoice()
+        let result = playRound(playerChoice, compChoice)
+        if (result.includes("You Win!") == true) {
+            playScore++
+            console.log(`You: ${playScore} | Computer: ${compScore}`)
+        }
+        else if (result.includes("You Lose!") == true) {
+            compScore++
+            console.log(`You: ${playScore} | Computer: ${compScore}`)
+        }
+        i++
+        if (playScore == 3 && compScore < 3) {
+         console.log("Congratulations! You're the Winner!")   
+        }
+        else if (compScore == 3 && playScore < 3) {
+            console.log("Better luck next time!")
+        }
+    }
+}
